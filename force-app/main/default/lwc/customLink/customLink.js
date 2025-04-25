8f24eb744fe239a95b2a81dfc8edf78ea0270f63
dumako <asazumarisa@gmail.com>
2025-04-25 09:06:21 +0000

    test sample of async input
    trailhead通りにやったら下記エラーとなったためjsを修正した
    - `_` 始まりの変数を用意
    - get/set に `@api` を付与
    
    ``` bash
    ✔ Backed up original state in git stash (bb57c35)
    ⚠ Running tasks for staged files...
      ❯ package.json — 3 files
        ✔ **/*.{cls,cmp,component,css,html,js,json,md,page,trigger,xml,yaml,yml} — 3 files
        ❯ **/{aura,lwc}/**/*.js — 2 files
          ✖ eslint [FAILED]
    ↓ Skipped because of errors from tasks.
    ✔ Reverting to original state because of errors...
    ✔ Cleaning up temporary files...
    
    ✖ eslint:
    
    /salesforce/lwc-customlink/force-app/main/default/lwc/customLink/customLink.js
      8:5  error  Invalid reassignment of public property "unitNumber"  @lwc/lwc/no-api-reassignments
    
    ✖ 1 problem (1 error, 0 warnings)
    
    husky - pre-commit script failed (code 1)
    ```

--- force-app/main/default/lwc/customLink/customLink.js
+++ force-app/main/default/lwc/customLink/customLink.js
@@ -2,5 +2,18 @@ import { LightningElement, api } from "lwc";
 import { sum } from "./sum";
 
 export default class CustomLink extends LightningElement {
-  @api unitNumber = sum(2, 3);
+  _unitNumber = sum(2, 3);
+
+  @api
+  set unitNumber(value) {
+    this._unitNumber = value;
+  }
+
+  get unitNumber() {
+    return this._unitNumber;
+  }
+
+  handleChange(event) {
+    this._unitNumber = event.target.value;
+  }
 }
